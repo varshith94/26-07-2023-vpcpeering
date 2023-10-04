c08e7bc4370d788bd79e3c026878b5b3f31426ae
varshith94 <104202580+varshith94@users.noreply.github.com>
2023-10-04 13:00:07 +0530

    Update peering.tf

--- peering.tf
+++ peering.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc_peering_connection" "owners" {
+resource "aws_vpc_peering_connection" "owner" {
     peer_owner_id = "002029865983"
     peer_vpc_id = aws_vpc.prod.id
     vpc_id = aws_vpc.dev.id
@@ -11,7 +11,7 @@ resource "aws_vpc_peering_connection" "owners" {
 }
  resource "aws_vpc_peering_connection_accepter" "accepter" {
     provider = aws.central
-    vpc_peering_connection_id = aws_vpc_peering_connection.owners.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.owner.id
     auto_accept = true
     
     tags = {
